058f03fa531c128dc0daa93cf3970ac9cb345461
naouel89 <jessus.n@gmail.COM>
2023-10-09 13:52:41 +0200

    V

--- connexion_script.php
+++ connexion_script.php
@@ -2,19 +2,19 @@
   <?php
   
     $host = "localhost"; // Remplacez par le nom d'hôte de votre base de données
-    $username = "root"; // Remplacez par votre nom d'utilisateur
+    $username = "jessus"; // Remplacez par votre nom d'utilisateur
     $password = "1234"; // Remplacez par votre mot de passe
-    $dbname = "dist"; // Remplacez par le nom de votre base de données
+    $dbname = "jessus"; // Remplacez par le nom de votre base de données
  
  // Établir la connexion à la base de données
-     $db = new PDO('mysql:host=localhost;charset=utf8;dbname=dist', 'root', '1234');
+     $db = new PDO('mysql:host=localhost;charset=utf8;dbname=jessus', 'jessus', '1234');
      $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
  
  
      try {
-      $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
+      $db = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
       // Gestion des erreurs PDO
-        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
     } catch (PDOException $e) {
         echo "Erreur de connexion à la base de données : " . $e->getMessage();
