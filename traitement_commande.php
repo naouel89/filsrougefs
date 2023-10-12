a02ab4466b18e02f41e7ab755b367655650b71d8
naouel89 <jessus.n@gmail.COM>
2023-10-12 10:25:09 +0200

    TR

--- traitement_commande.php
+++ traitement_commande.php
@@ -7,6 +7,21 @@ session_start();
 
 include('connexion_script.php');
 
+try {
+    $db = new PDO("mysql:host=$servername;dbname=$dbname;charset=utf8", $username, $password);
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die("La connexion à la base de données a échoué : " . $e->getMessage());
+}
+require_once 'Panier.php';
+
+// Inclure la bibliothèque PHPMailer
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+
+require 'vendor/autoload.php';
+
 function insertDataCommande($db, $id_plat, $quantite, $nom_client, $tel_client, $email_client, $adresse_client)
 {
     try {
@@ -50,7 +65,53 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
 
 
+// Créer une instance de PHPMailer
+$mail = new PHPMailer(true);
+
+try {
+    // Paramètres SMTP pour Gmail
+    $mail->isSMTP();
+    $mail->Host = 'smtp.gmail.com';
+    $mail->Port = 587;
+    $mail->SMTPAuth = true;
+    $mail->SMTPSecure = 'tls';
+
+    // Votre adresse Gmail et mot de passe
+    $mail->Username = 'distrcitdistrict@gmail.com';
+    $mail->Password = 'ggkp nkjm plmo kpwd';
+
+    // Destinataire de l'e-mail
+    $mail->setFrom('distrcitdistrict@gmail.com', 'Enzo');
+    $mail->addAddress($email, $nom); // Ajouter l'adresse du destinataire
+    $mail->isHTML(true); // Activer le support HTML
+
+    // Sujet et corps de l'e-mail
+    $mail->Subject = 'Confirmation de commande';
+    $mail->Body = 'Cher ' . $nom . ',<br><br>';
+    $mail->Body .= 'Votre commande a été enregistrée avec succès. Merci de votre achat !<br><br>';
+    foreach ($_SESSION['panier'] as $plat_id => $quantite) {
+        $plat = getPlatDetailsById($db, $plat_id);
+
+        if ($plat) {
+            $mail->Body .= 'Nom de l\'article : ' . $plat['libelle'] . '<br>';
+            $mail->Body .= 'Quantité : ' . $quantite . '<br>';
+            $mail->Body .= 'Prix unitaire : $' . $plat['prix'] . '<br>';
+            $mail->Body .= 'Sous-total : $' . ($quantite * $plat['prix']) . '<br><br>';
+        }
+    }
+    $mail->Body .= 'Montant total de la commande : $' . $montant_total . '<br><br>';
+    $mail->Body .= 'Cordialement,<br>Enzo';
+
+    // Envoyer l'e-mail
+    $mail->send();
+    unset($_SESSION['panier']); // Effacer le panier après l'envoi
+    echo 'E-mail envoyé avec succès !';
+    header("Location: index.php");
+    exit();
+} catch (Exception $e) {
+    echo 'Erreur lors de l\'envoi de l\'e-mail : ' . $e->getMessage();
+}
+?>
 
 
-
-    ?>
+    
